6142905739f15a9eab9a0febbfca935db43bc590
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2025-01-07 10:12:29 +0600

    add type and data type

--- index.go
+++ index.go
@@ -2,7 +2,29 @@ package main
 
 import "fmt"
 
-func main() {
-	fmt.Println("hello world")
+// func main() {
+// 	fmt.Println("hello world")
 
+// }
+func main() {
+	
+// var student1 string = "John" //type is string
+//   var student2 = "Jane" //type is inferred
+//   x := 2 //type is inferred
+
+//   fmt.Println(student1)
+//   fmt.Println(student2)
+//   fmt.Println(x)
+var a string = "hello world" //type is inferred
+  var b int
+  var c bool
+
+  fmt.Println(a)
+  fmt.Println(b)
+  fmt.Println(c)
+  var i string = "Hello"
+  var j int = 15
+
+  fmt.Printf("i has value: %v and type: %T\n", i, i)
+  fmt.Printf("j has value: %v and type: %T", j, j)
 }
